07785d2c0caf5ffa21b381fb98b94dc6664cc86b
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2022-09-24 13:13:59 +0200

    pdo appel

--- entrainement.php
+++ entrainement.php
@@ -6,7 +6,8 @@ $mysqlDsn = 'mysql: host=localhost; dbname=training1';
 try {
     $pdo = new PDO($mysqlDsn, $db_password = 'root', $db_user = 'root');
 
-    echo 'ee';
+    foreach($pdo -> query ('SELECT * from tableaux') as $user);
+    echo $user ['painter'];
 }
 
 catch(PDOException $e) {
